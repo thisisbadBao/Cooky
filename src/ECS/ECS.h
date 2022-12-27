05fc16d9aa406af5ef10c51976b0b58dc7d96292
Yicheng. Bao <60834356+thisisbadBao@users.noreply.github.com>
2022-12-27 22:10:42 +0800

    😷Add component pool.

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -3,6 +3,8 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
 
 const unsigned int MAX_COMPONENTS = 32;
 
@@ -25,40 +27,94 @@ class Component: public IComponent {
 };
 
 class Entity {
-    private:
-        int id;
+private:
+    int id;
 
-    public:
-        Entity(int id): id(id) {};
-        Entity(const Entity &entity) = default;
-        int GetId() const;
+public:
+    Entity(int id): id(id) {};
+    Entity(const Entity &entity) = default;
+    int GetId() const;
 
-        Entity& operator=(const Entity& other) = default;
-        bool operator==(const Entity& other) const { return other.GetId() == id; }
-        bool operator!=(const Entity& other) const { return other.GetId() != id; }
+    Entity& operator=(const Entity& other) = default;
+    bool operator==(const Entity& other) const { return other.GetId() == id; }
+    bool operator!=(const Entity& other) const { return other.GetId() != id; }
 };
 
 // The system processes entities that contain a specific signature
 class System {
-    private:
-        Signature componentSignature;
-        std::vector<Entity> entities;
+private:
+    Signature componentSignature;
+    std::vector<Entity> entities;
 
-    public:
-        System() = default;
-        ~System() = default;
+public:
+    System() = default;
+    ~System() = default;
 
-        void AddEntityToSystem(Entity entity);
-        void RemoveEntityFromSystem(Entity entity);
-        std::vector<Entity> GetSystemEntities() const;
-        const Signature& GetComponentSignature() const;
+    void AddEntityToSystem(Entity entity);
+    void RemoveEntityFromSystem(Entity entity);
+    std::vector<Entity> GetSystemEntities() const;
+    const Signature& GetComponentSignature() const;
 
-        // The component type that entities must have to be considered by the system
-        template <typename TComponent> void RequireComponent();
+    // The component type that entities must have to be considered by the system
+    template <typename TComponent> void RequireComponent();
 };
 
+class IPool {
+public:
+    virtual ~IPool() {}
+}
+
+// A vector of objects of type T
+template <typename T>
+class Pool: public IPool {
+private:
+    std::vector<T> data;
+
+public:
+    Pool(int size = 100) { Resize(size); }
+    virtual ~Pool() = default;
+
+    bool isEmpty() const { return data.empty(); }
+    int GetSize() const { return data.size(); }
+    void Resize(int n) { data.resize(n); }
+    void Clear() { data.clear(); }
+    void Add(T object) { data.push_back(object); }
+    void Set(int index, T object) { data[index] = object; }
+    T& Get(int index) { return static_cast<T &>(data[index]); }
+    T& operator[](unsigned int index) { return data[index]; }
+};
+
+// Manage the creation and destruction of entities, add systems, and components
 class Registry {
+private:
+    int numEntites = 0;
+
+    // Each pool contains all the data of a certain component
+    // vector index = component type id, Ipool index = entity id
+    std::vector<IPool*> componentPools;
+
+    // which component is "on" of each entity
+    // vector index = entity id
+    std::vector<Signature> entityComponentSigmatures;
+
+    // Map of active system
+    std::unordered_map<std::type_index, System*> systems;
+
+public:
+    Registry() = default;
+
     // Todo:
+    // CreateEntity();
+    // KillEntity()
+    //
+    // AddComponent(Entity entity)
+    // RemoveComponent(Entity entity)
+    // HasComponent(Entity entity)
+    //
+    // AddSystem()
+    // RemoveSystem()
+    // HasSystem()
+    // GetSystem()
 };
 
 #endif
